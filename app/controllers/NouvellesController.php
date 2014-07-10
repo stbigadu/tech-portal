edac0856e6d7335ffdc548ad48acb80e4cbe2817
Minh Nhat Bui <bui.minhnhat@gmail.com>
2014-07-09 23:14:25 -0400

    Nouvelles Module: setting up the Nouvelles controller

--- app/controllers/NouvellesController.php
+++ app/controllers/NouvellesController.php
@@ -39,16 +39,20 @@ class NouvellesController extends \BaseController {
 	    
 	    // Array of data to send to view
 	    $data = array(
-            'articles'      => $articles,
-            'ItemsCount'    => \T4KModels\Nouvelle::count(),
-            'currentRoute'  => \Route::currentRouteName(),
-            'activeScreen'  => 'NouvellesIndex'
+                'articles'      => $articles,
+                'ItemsCount'    => \T4KModels\Nouvelle::count(),
+                'currentRoute'  => \Route::currentRouteName(),
+                'activeScreen'  => 'NouvellesIndex'
 	    );
 	    
 	    // Render view
 	    $this->layout->content = \View::make('nouvelles.index', $data);
 	}
 	
+	/**
+	 * View a news item.
+	 * @param int $id
+	 */
 	public function view($id)
 	{
 	    // Retrieve all news
@@ -61,15 +65,148 @@ class NouvellesController extends \BaseController {
 	     
 	    // Array of data to send to view
 	    $data = array(
-            'article'       => $article,
-            'articles'      => $articles,
-            'ItemsCount'    => \T4KModels\Nouvelle::count(),
-            'currentRoute'  => \Route::currentRouteName(),
-            'activeScreen'  => 'NouvellesIndex'
+                'article'       => $article,
+                'articles'      => $articles,
+                'ItemsCount'    => \T4KModels\Nouvelle::count(),
+                'currentRoute'  => \Route::currentRouteName(),
+                'activeScreen'  => 'NouvellesIndex'
 	    );
 	     
 	    // Render view
 	    $this->layout->content = \View::make('nouvelles.index', $data);
+	}
+	
+	/**
+	 * Create a news item.
+	 * @return View Response
+	 */
+	public function create()
+	{
+	    // Array of data to send to view
+	    $data = array(
+                'currentRoute'  => \Route::currentRouteName(),
+                'activeScreen'  => 'NouvellesIndex'
+	    );
+	     
+	    // Render view
+	    $this->layout->content = \View::make('nouvelles.form', $data);
+	}
+	
+	/**
+	 * Post the new news item in DB.
+	 * @return Response
+	 */
+	public function store()
+	{
+	    // Validation rules
+	    $validator = Validator::make(Input::all(), \T4KModels\Nouvelle::$rules, \T4KModels\Nouvelle::$messages);
+	
+	    // Validator check
+	    if ($validator->fails())
+	    {
+	        // Throw error and redirect to previous screen
+	        return Redirect::route('admin.nouvelles.create')->withErrors($validator)->withInput();
+	    }
+	    else
+	    {
+	        // Create new object from model and save it
+	        $article = new \T4KModels\Nouvelle;
+	        $article->user_id      = Auth::user()->id;
+	        $article->datetime     = date('Y-m-d H:i:s');
+	        $article->title        = Input::get('title');
+	        $article->content      = Input::get('content');
+	        $article->save();
+	
+	        // Redirect to view screen with success message
+	        Session::flash('store', true);
+	        return Redirect::route('admin.nouvelles.view', $article->id);
+	    }
+	}
+	
+	/**
+	 * Modify an existing news item.
+	 * @param int $id
+	 * @return View Response
+	 */
+	public function edit($id)
+	{
+	    // Retrieve the news item with its id
+	    $article = \T4KModels\Nouvelle::find($id);
+	     
+	    // Array of data to send to view
+	    $data = array(
+                'article'       => $article,
+	            'currentRoute'  => \Route::currentRouteName(),
+                'activeScreen'  => 'NouvellesIndex'
+	    );
+	     
+	    // Render view
+	    $this->layout->content = \View::make('nouvelles.form', $data);
+	}
+	
+	/**
+	 * Post the updated news item to the DB.
+	 * @param int $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+	    // Validation rules
+	    $validator = Validator::make(Input::all(), \T4KModels\Nouvelle::$rules, \T4KModels\Nouvelle::$messages);
+	
+	    // Validator check
+	    if ($validator->fails())
+	    {
+	        // Throw error and redirect to previous screen
+	        return Redirect::route('portal.nouvelles.edit', $id)->withErrors($validator)->withInput();
+	    }
+	    else
+	    {
+	        // Retrieve object from model and update it
+	        $article = \T4KModels\Nouvelle::find($id);
+	        $article->title        = Input::get('title');
+	        $article->content      = Input::get('content');
+	        $article->save();
+	
+	        // Redirect to view screen with success message
+	        Session::flash('update', true);
+	        return Redirect::route('portal.nouvelles.view', $article->id);
+	    }
+	}
+	
+	/**
+	 * Soft destroy a news item.
+	 * @param int $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+	    // Retrieve object
+	    $article = \T4KModels\Nouvelle::where('id', $id)->first();
+	    Session::flash('object_name', $article->title_FR);
+	    
+	    // Delete object
+	    $article->delete();
+	
+	    // Redirect to view screen with success message
+	    Session::flash('destroy', true);
+	    return Redirect::route('admin.nouvelles.index');
+	}
+	
+	/**
+	 * Catch-all method for handling missing methods.
+	 * @return Redirect Response
+	 */
+	public function missingMethod($parameters = array())
+	{
+	    // Array of data to send to view
+	    $data = array(
+                'currentRoute'  => \Route::currentRouteName(),
+                'activeScreen'  => 'NouvellesIndex'
+	    );
+	    
+	    // Redirect to Dashboard
+	    return Redirect::route('admin.nouvelles.index', $data);
 	}
 
 }
